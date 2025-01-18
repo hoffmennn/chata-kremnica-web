ba5fdabddbd081eacabaf7dec2d41580153cdf47
hoffmennn <adam.hoffmann.151@gmail.com>
2025-01-18 22:00:29 +0100

    pridanie samostatneho suboru gallery_script.js

---
+++ js/gallery_script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  $("#in_button").click(function(){
+    $("#ex_container").fadeToggle("fast");
+    $("#in_container").fadeToggle("fast");
+  });
+});
+
+
+$(document).ready(function () {
+$(".toggle-btn").click(function () {
+// Toggle the "active" class between the two spans
+$(this).find("span").toggleClass("active");
+});
+});
+
